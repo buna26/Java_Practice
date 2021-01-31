d2e242ee3163de9588d949a558062f91a45e4b2e
Riya Das <71592945+buna26@users.noreply.github.com>
2021-01-31 19:52:27 +0530

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+class Solution{
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(N+" x "+i+" = "+N*i); 
+        }
+
+    }
+}
