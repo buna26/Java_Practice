1a56ea549e9f2e99c7d13dbd2ff3e6628f3a45ce
Riya Das <71592945+buna26@users.noreply.github.com>
2021-02-01 16:33:50 +0530

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while(scan.hasNext()){
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
