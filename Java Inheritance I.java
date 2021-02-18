4dd464aa64ca907b01a09f87dedaefb334e1ae3b
Riya Das <71592945+buna26@users.noreply.github.com>
2021-02-18 20:50:45 +0530

    Create Java Inheritance I.java

---
+++ Java Inheritance I.java
@@ -0,0 +1,11 @@
+class Insect extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+}
+
+class Bird extends Insect {
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
