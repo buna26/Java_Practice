eb65390964d02408bddc50f6effa790aaa4eef30
Riya Das <71592945+buna26@users.noreply.github.com>
2021-03-08 10:19:46 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,8 @@
+static int camelcase(String s) {
+        int count =1;
+         for (int i=0; i<s.length(); i++){
+            if (Character.isUpperCase(s.charAt(i)))
+            count ++;
+        }
+        return count;
+    }
