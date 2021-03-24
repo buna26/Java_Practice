19660b12cced11c7b73c7f55984aa300aee2721b
Riya Das <71592945+buna26@users.noreply.github.com>
2021-03-24 15:59:58 +0530

    Create Number Line Jumps.java

---
+++ Number Line Jumps.java
@@ -0,0 +1,9 @@
+if (v1 > v2) {
+            
+        int remainder = (x1 - x2) % (v2 - v1);
+            
+        if (remainder == 0) {
+            return "YES";
+            }
+        }
+        return "NO";
